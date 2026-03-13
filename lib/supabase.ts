c60e9f5f63b261a23d8b097d8b37a4cfe94e1bdf
Sirsalmonsito21 <rodrigotamayo2005@gmail.com>
2026-03-12 20:37:33 -0500

    conectar supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://alzcnpfdhjidfuxjumim.supabase.co',
+  'sb_publishable_i13ry-cmmlgqFUEFST5e0g_7PtJop_p'
+)
